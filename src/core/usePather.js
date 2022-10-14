f4e71ef21826159f5f163443a10b6cdf339d18b3
qwerty34-qw <wh861440@gmail.com>
2022-10-14 17:03:42 +0300

    Delete unnecessary imports

--- src/core/usePather.js
+++ src/core/usePather.js
@@ -1,6 +1,5 @@
 import { useContext } from 'react';
 import withPather from './with-pather';
-import { useNavigate, useLocation } from 'react-router-dom'
 import PatherContext from './pather-context';
 
 
